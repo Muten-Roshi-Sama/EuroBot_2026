534e48557e97b95841928076242a2dd6e7f668ce
Muten-Roshi-Sama <21245@ecam.be>
2025-10-08 15:05:37 +0200

    PlatformIO project Setup.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+// v0.0
+
+
+//  TODO :
+//        - Fix 
+//        - Add 
+//        -
+//        - 
+
+#include <Arduino.h>
+#include "FSM.h"
+// #include "Movement.h"
+
+
+
+void setup() {
+  Serial.println("Setup.");
+
+}
+
+
+
+void loop(){
+
+
+}
+
+
+
+
