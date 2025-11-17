5b9ffe3219c1472d15432d94214a4ad8d5b1df20
Felix0427d <felixdecrouychanel@hotmail.com>
2025-11-17 11:07:50 +0100

    Bouton d'arrêt d'urgence librairie fonctionnelle

--- lib/fsm/FSM.cpp
+++ lib/fsm/FSM.cpp
@@ -1,86 +1,37 @@
 
-#ifdef ARDUINO
-#include <Arduino.h>
-#else
-#include <stdint.h>
-static inline unsigned long millis() { return 0; }
-#endif
+
 #include "FSM.h"
-
-static void markStateStart(FsmContext &ctx) {
-  ctx.stateStartMs = millis();
-}
-
-void fsmChangeAction(FsmContext &ctx, FsmAction next) {
-  ctx.currentAction = next;
-  markStateStart(ctx);
-}
 
 void fsmInit(FsmContext &ctx) {
   ctx.currentAction = FsmAction::INIT;
-  markStateStart(ctx);
 }
-
-// emergency flag (set by fsmTriggerEmergency)
-static volatile bool gEmergencyFlag = false;
 
 void fsmStep(FsmContext &ctx) {
-  // Priority: emergency stop preempts any state
-  if (gEmergencyFlag && ctx.currentAction != FsmAction::EMERGENCY_STOP) {
-    fsmEmergencyStop(ctx);
-    // Once we force the stop, clear the flag so it doesn't retrigger endlessly
-    gEmergencyFlag = false;
-    return;
-  }
-
   switch (ctx.currentAction) {
-    case FsmAction::INIT: {
+    case FsmAction::INIT:
       // TODO: initialize subsystems, wait for start condition
-      // Example transition placeholder:
-      // if (startCondition) fsmChangeAction(ctx, FsmAction::MOVE_FORWARD);
       break;
-    }
-    case FsmAction::EMERGENCY_STOP: {
-      // TODO: immediate stop of all actuators; keep safe state
-      // Remain here until reset condition
-      break;
-    }
-    case FsmAction::MOVE_FORWARD: {
+    case FsmAction::MOVE_FORWARD:
       // TODO: command motors to move forward
-      // Transition when done:
-      // fsmChangeAction(ctx, FsmAction::MOVE_BACKWARD);
       break;
-    }
-    case FsmAction::MOVE_BACKWARD: {
+    case FsmAction::MOVE_BACKWARD:
       // TODO: command motors to move backward
-      // Transition when done:
-      // fsmChangeAction(ctx, FsmAction::TURN_AROUND);
       break;
-    }
-    case FsmAction::TURN_AROUND: {
+    case FsmAction::TURN_AROUND:
       // TODO: rotate robot 180 degrees
-      // Transition when done:
-      // fsmChangeAction(ctx, FsmAction::END);
       break;
-    }
-    case FsmAction::END: {
+    case FsmAction::END:
       // TODO: stop motors, finalize
-      // Remain in END or reset to INIT as needed
       break;
-    }
-    default: {
-      // Safety: reset to INIT on unknown action
-      fsmChangeAction(ctx, FsmAction::INIT);
+    case FsmAction::EMERGENCY_STOP:
+      // Appel à la librairie EmergencyStop
+      if (emergency_button_pressed()) {
+        // Ici, on peut ajouter la logique d'arrêt d'urgence (ex: couper moteurs, etc)
+        // Serial.println("Arrêt d'urgence déclenché !");
+      }
       break;
-    }
+    default:
+      ctx.currentAction = FsmAction::INIT;
+      break;
   }
-}
-
-void fsmEmergencyStop(FsmContext &ctx) {
-  // Force transition to EMERGENCY_STOP
-  fsmChangeAction(ctx, FsmAction::EMERGENCY_STOP);
-}
-
-void fsmTriggerEmergency() {
-  gEmergencyFlag = true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,11 @@
-// v0.0
 
-
-//  TODO :
-//        - Fix 
-//        - Add 
-//        -
-//        - 
-
-// v0.1 wiring FSM minimal usage
-#if __has_include(<Arduino.h>)
 #include <Arduino.h>
-#else
-// Minimal mock for non-Arduino analysis/linting
-struct MockSerial {
-  void begin(unsigned long) {}
-  void println(const char*) {}
-  explicit operator bool() const { return true; }
-} Serial;
-static inline void delay(unsigned long) {}
-#endif
-#include "C:/Users/felix/Documents/Master 1 ECAM/Embbeded_project/Projet_git/EuroBot_2026/lib/fsm/FSM.h"
-
-
-static FsmContext gFsm; 
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {}
-  Serial.println("Setup.");
-  
-  fsmInit(gFsm);
 }
 
-
-
-void loop(){
-
-  fsmStep(gFsm);
+void loop() {
 }
 
 
